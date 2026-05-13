849c50bc6894ef477d9f462e1fc32cf1feda98d6
JohnnyVicious <6608571+JohnnyVicious@users.noreply.github.com>
2026-05-13 23:16:40 +0200

    Revert "Annotate debounce manager CLS surface"
    This reverts commit eb897df2a590116c9cfddd55d10df44930acd491.

--- src/NzbDrone.Common/TPL/DebounceManager.cs
+++ src/NzbDrone.Common/TPL/DebounceManager.cs
@@ -2,13 +2,11 @@ using System;
 
 namespace NzbDrone.Common.TPL
 {
-    [CLSCompliant(false)]
     public interface IDebounceManager
     {
         Debouncer CreateDebouncer(Action action, TimeSpan debounceDuration);
     }
 
-    [CLSCompliant(false)]
     public class DebounceManager : IDebounceManager
     {
         public Debouncer CreateDebouncer(Action action, TimeSpan debounceDuration)
