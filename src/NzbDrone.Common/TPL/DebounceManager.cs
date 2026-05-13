eb897df2a590116c9cfddd55d10df44930acd491
JohnnyVicious <6608571+JohnnyVicious@users.noreply.github.com>
2026-05-13 23:12:05 +0200

    Annotate debounce manager CLS surface

--- src/NzbDrone.Common/TPL/DebounceManager.cs
+++ src/NzbDrone.Common/TPL/DebounceManager.cs
@@ -2,11 +2,13 @@ using System;
 
 namespace NzbDrone.Common.TPL
 {
+    [CLSCompliant(false)]
     public interface IDebounceManager
     {
         Debouncer CreateDebouncer(Action action, TimeSpan debounceDuration);
     }
 
+    [CLSCompliant(false)]
     public class DebounceManager : IDebounceManager
     {
         public Debouncer CreateDebouncer(Action action, TimeSpan debounceDuration)
