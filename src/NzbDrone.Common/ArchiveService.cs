5590ae2c88c92aece8d82911aab53b970309f57a
JohnnyVicious <6608571+JohnnyVicious@users.noreply.github.com>
2026-05-13 20:13:02 +0200

    Secure archive extraction paths

--- src/NzbDrone.Common/ArchiveService.cs
+++ src/NzbDrone.Common/ArchiveService.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 using ICSharpCode.SharpZipLib.Core;
 using ICSharpCode.SharpZipLib.GZip;
 using ICSharpCode.SharpZipLib.Tar;
 using ICSharpCode.SharpZipLib.Zip;
+using NzbDrone.Common.Disk;
 using NLog;
 
 namespace NzbDrone.Common
@@ -58,6 +61,8 @@ namespace NzbDrone.Common
 
         private void ExtractZip(string compressedFile, string destination)
         {
+            Directory.CreateDirectory(destination);
+
             using (var fileStream = File.OpenRead(compressedFile))
             {
                 var zipFile = new ZipFile(fileStream);
@@ -71,12 +76,18 @@ namespace NzbDrone.Common
 
                 foreach (ZipEntry zipEntry in zipFile)
                 {
+                    var entryFileName = zipEntry.Name;
+                    var fullZipToPath = GetSafeExtractionPath(destination, entryFileName);
+
+                    if (IsZipSymlink(zipEntry))
+                    {
+                        throw new IOException($"Archive entry '{entryFileName}' is not a regular file.");
+                    }
+
                     if (!zipEntry.IsFile)
                     {
                         continue; // Ignore directories
                     }
-
-                    var entryFileName = zipEntry.Name;
 
                     // to remove the folder from the entry:- entryFileName = Path.GetFileName(entryFileName);
                     // Optionally match entrynames against a selection list here to skip as desired.
@@ -86,7 +97,6 @@ namespace NzbDrone.Common
                     var zipStream = zipFile.GetInputStream(zipEntry);
 
                     // Manipulate the output filename here as desired.
-                    var fullZipToPath = Path.Combine(destination, entryFileName);
                     var directoryName = Path.GetDirectoryName(fullZipToPath);
                     if (directoryName.Length > 0)
                     {
@@ -106,15 +116,109 @@ namespace NzbDrone.Common
 
         private void ExtractTgz(string compressedFile, string destination)
         {
-            Stream inStream = File.OpenRead(compressedFile);
-            Stream gzipStream = new GZipInputStream(inStream);
+            Directory.CreateDirectory(destination);
 
-            var tarArchive = TarArchive.CreateInputTarArchive(gzipStream, null);
-            tarArchive.ExtractContents(destination);
-            tarArchive.Close();
+            using (Stream inStream = File.OpenRead(compressedFile))
+            using (Stream gzipStream = new GZipInputStream(inStream))
+            using (var tarStream = new TarInputStream(gzipStream, Encoding.UTF8))
+            {
+                TarEntry tarEntry;
+                while ((tarEntry = tarStream.GetNextEntry()) != null)
+                {
+                    var fullPath = GetSafeExtractionPath(destination, tarEntry.Name);
 
-            gzipStream.Close();
-            inStream.Close();
+                    if (tarEntry.IsDirectory)
+                    {
+                        Directory.CreateDirectory(fullPath);
+                        continue;
+                    }
+
+                    if (!IsRegularTarEntry(tarEntry))
+                    {
+                        throw new IOException($"Archive entry '{tarEntry.Name}' is not a regular file.");
+                    }
+
+                    var directoryName = Path.GetDirectoryName(fullPath);
+                    if (directoryName.Length > 0)
+                    {
+                        Directory.CreateDirectory(directoryName);
+                    }
+
+                    using (var streamWriter = File.Create(fullPath))
+                    {
+                        tarStream.CopyEntryContents(streamWriter);
+                    }
+                }
+            }
+        }
+
+        private static string GetSafeExtractionPath(string destination, string entryName)
+        {
+            if (string.IsNullOrWhiteSpace(entryName))
+            {
+                throw new IOException("Archive entry has an invalid file name.");
+            }
+
+            var normalizedEntryName = entryName.Replace('\\', '/');
+
+            if (Path.IsPathRooted(entryName) ||
+                normalizedEntryName.StartsWith("/", StringComparison.Ordinal) ||
+                normalizedEntryName.Contains(":", StringComparison.Ordinal))
+            {
+                throw new IOException($"Archive entry '{entryName}' targets a path outside the extraction folder.");
+            }
+
+            var pathSegments = normalizedEntryName.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            if (!pathSegments.Any() || pathSegments.Any(s => s == "." || s == ".."))
+            {
+                throw new IOException($"Archive entry '{entryName}' targets a path outside the extraction folder.");
+            }
+
+            var destinationPath = Path.GetFullPath(destination);
+            var destinationWithSeparator = EnsureTrailingDirectorySeparator(destinationPath);
+            var combinedPath = Path.Combine(new[] { destinationPath }.Concat(pathSegments).ToArray());
+            var fullPath = Path.GetFullPath(combinedPath);
+
+            if (!fullPath.StartsWith(destinationWithSeparator, DiskProviderBase.PathStringComparison))
+            {
+                throw new IOException($"Archive entry '{entryName}' targets a path outside the extraction folder.");
+            }
+
+            return fullPath;
+        }
+
+        private static string EnsureTrailingDirectorySeparator(string path)
+        {
+            if (path.EndsWith(Path.DirectorySeparatorChar) || path.EndsWith(Path.AltDirectorySeparatorChar))
+            {
+                return path;
+            }
+
+            return path + Path.DirectorySeparatorChar;
+        }
+
+        private static bool IsZipSymlink(ZipEntry zipEntry)
+        {
+            const int unixHostSystem = 3;
+            const int unixFileTypeMask = 0xF000;
+            const int unixSymlinkFileType = 0xA000;
+
+            if (zipEntry.HostSystem != unixHostSystem)
+            {
+                return false;
+            }
+
+            var unixMode = (zipEntry.ExternalFileAttributes >> 16) & unixFileTypeMask;
+
+            return unixMode == unixSymlinkFileType;
+        }
+
+        private static bool IsRegularTarEntry(TarEntry tarEntry)
+        {
+            var typeFlag = tarEntry.TarHeader.TypeFlag;
+
+            return typeFlag == TarHeader.LF_OLDNORM ||
+                   typeFlag == TarHeader.LF_NORMAL;
         }
 
         private void OnZipError(TestStatus status, string message)
--- src/NzbDrone.Core.Test/ProviderTests/DiskProviderTests/ArchiveProviderFixture.cs
+++ src/NzbDrone.Core.Test/ProviderTests/DiskProviderTests/ArchiveProviderFixture.cs
@@ -1,5 +1,9 @@
 ﻿using System.IO;
+using System.Text;
 using FluentAssertions;
+using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Tar;
+using ICSharpCode.SharpZipLib.Zip;
 using NUnit.Framework;
 using NzbDrone.Common;
 using NzbDrone.Common.EnvironmentInfo;
@@ -22,6 +26,101 @@ namespace NzbDrone.Core.Test.ProviderTests.DiskProviderTests
             destinationFolder.GetDirectories().Should().HaveCount(1);
             destinationFolder.GetDirectories("*", SearchOption.AllDirectories).Should().HaveCount(3);
             destinationFolder.GetFiles("*.*", SearchOption.AllDirectories).Should().HaveCount(6);
+        }
+
+        [Test]
+        public void should_extract_zip_entries_inside_destination()
+        {
+            var destination = Path.Combine(TempFolder, "restore");
+            var archive = CreateZipArchive("folder/file.txt", "valid");
+
+            Subject.Extract(archive, destination);
+
+            File.ReadAllText(Path.Combine(destination, "folder", "file.txt")).Should().Be("valid");
+        }
+
+        [TestCase("../escape.txt")]
+        [TestCase("folder/../../escape.txt")]
+        [TestCase("/escape.txt")]
+        [TestCase(@"C:\escape.txt")]
+        [TestCase(@"folder\..\escape.txt")]
+        public void should_reject_zip_entries_outside_destination(string entryName)
+        {
+            var destination = Path.Combine(TempFolder, "restore");
+            var archive = CreateZipArchive(entryName, "blocked");
+
+            Subject.Invoking(s => s.Extract(archive, destination))
+                   .Should().Throw<IOException>();
+
+            File.Exists(Path.Combine(TempFolder, "escape.txt")).Should().BeFalse();
+        }
+
+        [Test]
+        public void should_extract_tgz_entries_inside_destination()
+        {
+            var destination = Path.Combine(TempFolder, "restore");
+            var archive = CreateTgzArchive("folder/file.txt", "valid");
+
+            Subject.Extract(archive, destination);
+
+            File.ReadAllText(Path.Combine(destination, "folder", "file.txt")).Should().Be("valid");
+        }
+
+        [TestCase("../escape.txt")]
+        [TestCase("folder/../../escape.txt")]
+        [TestCase("/escape.txt")]
+        [TestCase(@"C:\escape.txt")]
+        [TestCase(@"folder\..\escape.txt")]
+        public void should_reject_tgz_entries_outside_destination(string entryName)
+        {
+            var destination = Path.Combine(TempFolder, "restore");
+            var archive = CreateTgzArchive(entryName, "blocked");
+
+            Subject.Invoking(s => s.Extract(archive, destination))
+                   .Should().Throw<IOException>();
+
+            File.Exists(Path.Combine(TempFolder, "escape.txt")).Should().BeFalse();
+        }
+
+        private string CreateZipArchive(string entryName, string contents)
+        {
+            var path = GetTempFilePath() + ".zip";
+            var bytes = Encoding.UTF8.GetBytes(contents);
+
+            using (var fileStream = File.Create(path))
+            using (var zipStream = new ZipOutputStream(fileStream))
+            {
+                var zipEntry = new ZipEntry(entryName)
+                {
+                    Size = bytes.Length
+                };
+
+                zipStream.PutNextEntry(zipEntry);
+                zipStream.Write(bytes, 0, bytes.Length);
+                zipStream.CloseEntry();
+            }
+
+            return path;
+        }
+
+        private string CreateTgzArchive(string entryName, string contents)
+        {
+            var path = GetTempFilePath() + ".tar.gz";
+            var bytes = Encoding.UTF8.GetBytes(contents);
+
+            using (var fileStream = File.Create(path))
+            using (var gzipStream = new GZipOutputStream(fileStream))
+            using (var tarStream = new TarOutputStream(gzipStream, Encoding.UTF8))
+            {
+                var tarEntry = TarEntry.CreateTarEntry(entryName);
+                tarEntry.Size = bytes.Length;
+
+                tarStream.PutNextEntry(tarEntry);
+                tarStream.Write(bytes, 0, bytes.Length);
+                tarStream.CloseEntry();
+            }
+
+            return path;
         }
     }
 }
