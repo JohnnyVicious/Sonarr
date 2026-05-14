8fc76d13bed58f48346ddbfbb9bae6cb61efe379
JohnnyVicious <6608571+JohnnyVicious@users.noreply.github.com>
2026-05-14 16:17:22 +0200

    [fork] Document flat parseUrl query params

--- frontend/src/Utilities/String/parseUrl.ts
+++ frontend/src/Utilities/String/parseUrl.ts
@@ -1,6 +1,5 @@
 import _ from 'lodash';
 
-
 // See: https://developer.mozilla.org/en-US/docs/Web/API/HTMLHyperlinkElementUtils
 const anchor = document.createElement('a');
 
@@ -26,6 +25,8 @@ export default function parseUrl(url: string) {
   properties.isAbsolute = /^[\w:]*\/\//.test(url);
 
   if (properties.search) {
+    // Current callers use pathname/search for URL cleansing; params are kept
+    // as a flat best-effort map, not as a qs-compatible nested/array parser.
     properties.params = Object.fromEntries(
       new URLSearchParams(properties.search as string)
     );
